9cb460ffbf1bfbb937a0bd9f616bff346b62a9c0
Madhusudan <ind.madhusudan20@gmail.com>
2025-03-20 16:24:34 +0530

    contact us page added

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -18,15 +18,15 @@ const Contact = () => {
         />
         <div className="flex flex-col justify-center items-start gap-6 bg-[#D0BFFF] px-10 rounded-lg">
 
-          <p className=" font-semibold text-lg text-gray-600">OUR OFFICE</p>
+          <p className=" font-semibold text-lg text-gray-600">Our Office</p>
           <p className=" text-gray-500">
-            54709 Willms Station <br /> Suite 350, Washington, USA
+            9th Street, Besides Bokaro Mall <br /> A Block, Bokaro, India
           </p>
           <p className=" text-gray-500">
-            Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com
+            Phone: +91-8709887878  <br /> Email: pulsecure@gmail.com
           </p>
           <p className=" font-semibold text-lg text-gray-600">
-            CAREERS AT PRESCRIPTO
+            Careers at PulseCure
           </p>
           <p className=" text-gray-500">
             Learn more about our teams and job openings.
